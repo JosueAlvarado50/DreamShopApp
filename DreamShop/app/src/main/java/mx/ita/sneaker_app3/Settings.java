e67bb38c596fd3fc93103c4807961f53f3df6b9b
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2022-05-28 23:00:47 -0500

    cambios

--- DreamShop/app/src/main/java/mx/ita/sneaker_app3/Settings.java
+++ DreamShop/app/src/main/java/mx/ita/sneaker_app3/Settings.java
@@ -15,10 +15,7 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         noche = findViewById(R.id.buttonnoche);
-
-
         noche.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
