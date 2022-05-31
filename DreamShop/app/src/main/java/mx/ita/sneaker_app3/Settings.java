cb35e2708e4c9ecd43f57422650d6fa81131ee00
JosueAlvarado50 <84753662+JosueAlvarado50@users.noreply.github.com>
2022-05-31 16:18:28 -0500

    botones back

--- DreamShop/app/src/main/java/mx/ita/sneaker_app3/Settings.java
+++ DreamShop/app/src/main/java/mx/ita/sneaker_app3/Settings.java
@@ -3,6 +3,7 @@ package mx.ita.sneaker_app3;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,22 +11,28 @@ import android.widget.Button;
 public class Settings extends AppCompatActivity {
 
         private Button noche;
+        int nightMode = AppCompatDelegate.getDefaultNightMode();
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         noche = findViewById(R.id.buttonnoche);
+
         noche.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int nightMode = AppCompatDelegate.getDefaultNightMode();
+
                 if (nightMode == AppCompatDelegate.MODE_NIGHT_YES) {
                     AppCompatDelegate.setDefaultNightMode
                             (AppCompatDelegate.MODE_NIGHT_NO);
+                    nightMode = 1;
                 } else {
                     AppCompatDelegate.setDefaultNightMode
                             (AppCompatDelegate.MODE_NIGHT_YES);
+                    nightMode = 2;
                 }
 
             }
